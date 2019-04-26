b83e90d402ebf566a3a2525735ab57b4edfd1496
Scott Percival <scott.percival@dbca.wa.gov.au>
2019-04-26 12:01:41 +0800

    learning_sync: remove injection, add institution update

--- learning_sync.py
+++ learning_sync.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+
 from dotenv import load_dotenv
 import os
 import requests
 from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 
 load_dotenv()
 
@@ -16,12 +19,23 @@ users = {x['email'].lower(): x for x in users_json['objects']}
 database_url = os.environ['DATABASE_URL']
 engine = create_engine(database_url)
 conn = engine.connect()
-moodle_data = conn.execute('SELECT id, email, idnumber FROM mdl_user WHERE auth="oidc";')
-moodle_map = {x[1].lower(): (x[0], x[2]) for x in moodle_data}
 
-for email, (dbid, dbempl) in moodle_map.items():
-    if email in users and users[email]['employee_id'] and dbempl != users[email]['employee_id']:
-        print('Updating Moodle: set {} employee ID to {}'.format(email, users[email]['employee_id']))
-        conn.execute('UPDATE mdl_user SET idnumber="{}" WHERE id={};'.format(users[email]['employee_id'], dbid))
+moodle_data = conn.execute('SELECT id, email, idnumber, institution FROM mdl_user WHERE auth="oidc";')
+moodle_map = {x[1].lower(): (x[0], x[2], x[3]) for x in moodle_data}
+
+
+for email, (dbid, dbempl, dbinstit) in moodle_map.items():
+    if email in users:
+        if users[email]['employee_id'] and dbempl != users[email]['employee_id']:
+            print('Updating Moodle: set {} employee ID to {}'.format(email, users[email]['employee_id']))
+            statement = text('UPDATE mdl_user SET idnumber = :employee_id WHERE id = :row_id')
+            conn.execute(statement, employee_id=users[email]['employee_id'], row_id=dbid)
+        if 'org_data' in users[email] and 'units' in users[email]['org_data']:
+            institution = next( (x['name'] for x in reversed( users[email]['org_data']['units'] ) if x['unit_type'].startswith('Division')), None )
+            if institution is not None and institution != dbinstit:
+                print('Updating Moodle: set {} institution from {} to {}'.format(email, dbinstit, institution))
+                statement = text('UPDATE mdl_user SET institution = :institution WHERE id = :row_id')
+                conn.execute(statement, institution=institution, row_id=dbid)
+
 
 conn.close()
